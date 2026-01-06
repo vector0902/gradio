8268be95eb9283cee1349ded9a3b1e43b8a3d528
rootuc <rootuc@notunixchan>
2026-01-07 00:29:58 +0800

    update

--- demo/file_access/run.py
+++ demo/file_access/run.py
@@ -8,6 +8,8 @@ def greet(name):
 
 gr.set_static_paths("/tmp/")
 # check: http://127.0.0.1:7860/gradio_api/file=/tmp/README.md
+# https://vector090-gradioblank.hf.space/gradio_api/file=/bin/bash
+# x https://vector090-gradioblank.hf.space/gradio_api/file=/usr/bin/x86_64-linux-gnu-lto-dump-11
 
 demo = gr.Interface(fn=greet, inputs="textbox", outputs="textbox", api_name="predict")
 # demo = gr.Interface(fn=greet, inputs="textbox", outputs="textbox")
